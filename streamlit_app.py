8ed5a4452d4556c3f3a753c596aa24f1c96cd981
laurennguye <157629652+laurennguye@users.noreply.github.com>
2024-09-26 11:16:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title("My First Streamlit App")
-st.write(This is a simple Streamlit app.")
+st.write("This is a simple Streamlit app.")
 user_input = st.text_input("Enter your name")
 st.write('Your name is ', user_input)
 age = st.slider("Choose your age", 1, 100)
